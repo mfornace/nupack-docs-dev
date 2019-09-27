54ce13635d44942b4390e39c9b9c16ccf2c223fd
Mark Fornace <mfornace@caltech.edu>
2019-09-27 12:36:00 -0700

    update.sh

---
+++ update.sh
@@ -0,0 +1,12 @@
+git submodule add -b mkdocs https://github.com/mfornace/nupack.git
+rm -rf 22442bb9aba38be019268cfdd1e26d30ab22d9a2
+cp -r nupack 22442bb9aba38be019268cfdd1e26d30ab22d9a2
+rm -rf 22442bb9aba38be019268cfdd1e26d30ab22d9a2/.git
+git rm -r --force nupack
+rm -rf .git/modules/nupack/
+git add 22442bb9aba38be019268cfdd1e26d30ab22d9a2
+git commit -m "Update upstream branch"
+# git branch -D gh-pages
+# git push origin --delete gh-pages
+git pull
+git push
