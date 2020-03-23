b87737ef9ebfd9e616656b5f7b0c16c70c69c0a0
Mark Fornace <mfornace@caltech.edu>
2020-03-23 10:18:52 -0700

    Update master

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
-git submodule add -b mkdocs https://github.com/mfornace/nupack.git
+git submodule add -b build https://github.com/mfornace/nupack-documentation.git tmp
 rm -rf 22442bb9aba38be019268cfdd1e26d30ab22d9a2
-cp -r nupack 22442bb9aba38be019268cfdd1e26d30ab22d9a2
+cp -r tmp 22442bb9aba38be019268cfdd1e26d30ab22d9a2
 rm -rf 22442bb9aba38be019268cfdd1e26d30ab22d9a2/.git
-git rm -r --force nupack
-rm -rf .git/modules/nupack/
+git rm -r --force tmp
+rm -rf .git/modules/nupack-documentation/
 git add 22442bb9aba38be019268cfdd1e26d30ab22d9a2
 git commit -m "Update upstream branch"
 # git branch -D gh-pages
