2f7d1c7d0477453e9aea238fbfbdaa16662cb822
Mark Fornace <mfornace@caltech.edu>
2020-12-02 08:56:16 -0800

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-git pull upstream revise-names && cd sources && zip -r examples.zip examples && cd .. && mkdocs gh-deploy
+git pull && cd sources && zip -r examples.zip examples && cd .. && mkdocs gh-deploy -r deploy
